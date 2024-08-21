312146be68628239130df89243d5a5352ee515b9
mezoxy <ayoub.mezhar@usmba.ac.ma>
2024-08-21 13:58:11 +0100

    task 1

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+'''1-fifo_cache.py'''
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    '''BasicCache defines:
+        - Caching system
+    '''
+    def put(self, key, item):
+        '''
+            put: A methode that assign an item to the dic self.cache_data
+            Args:
+                key: The key of the item
+                item: The item to add in self.cache_data
+        '''
+        if key and item:
+            if len(self.cache_data.keys()) < BaseCaching.MAX_ITEMS:
+                self.cache_data[key] = item
+            else:
+                print(f'DISCARD: {list(self.cache_data.keys())[0]}')
+                del self.cache_data[list(self.cache_data.keys())[0]]
+                self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+            get: A method that retrive an item by key
+
+            Args:
+                key: The key of the item to retrive
+
+            Return: The item if the key and the item are not None
+        '''
+        try:
+            if key:
+                return self.cache_data[key]
+        except Exception as e:
+            return None
